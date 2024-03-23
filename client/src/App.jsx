275450bc0b48d74d16682e52f7d0d90ed1d8d733
Vedant Sanap <vedantsanap123@gmail.com>
2024-03-23 16:35:37 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
 
-function App() {
-  return (
-    <div>
-      {/* nav cha component */}
-      
-    </div>
-  )
+import React, { Component } from 'react'
+
+import Navbar from './Components/Navbar';
+
+import { Content } from './Components/Content'
+
+import './App.css'
+
+export class App extends Component {
+  render() {
+    return (
+     <>
+     
+     <Navbar />
+     <Content />
+     </>
+    )
+  }
 }
 
 export default App
