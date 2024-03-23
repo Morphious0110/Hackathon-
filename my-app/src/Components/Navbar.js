a7d6f53779a9fa2178e5142c85727f9b14cd60e9
Vedant Sanap <vedantsanap123@gmail.com>
2024-03-23 15:44:09 +0530

    navbar

---
+++ my-app/src/Components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { BrowserRouter as Router, Route,  Link } from 'react-router-dom';
+import { Switch } from 'react-router-dom'
+import Home from '../Components/Home';
+import About from '../Components/About';
+import Contact from '../Components/Contact';
+
+function Navbar() {
+  return (
+   
+      <Router>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">FloraSense</a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/Home">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <Switch>
+
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/Home">
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
+   
+  );
+}
+
+export default Navbar;
